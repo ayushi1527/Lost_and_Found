12def47e9b085b7042a8049e6e4ff72861ee5e23
Ayushi Thakur <somyathakur1527@gmial.com>
2026-02-16 23:28:12 +0530

    new api changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,12 @@
-// export const API_BASE = import.meta.env.VITE_API_URL;
+export const API_BASE = import.meta.env.VITE_API_URL;
 
-// export function apiUrl(path) {
-//   return `${API_BASE}${path}`;
-// }
-// export function getAuthHeaders(contentType = "application/json") {
-//   const headers = {};
-//   const token = localStorage.getItem("token");
-//   if (contentType) headers["Content-Type"] = contentType;
-//   if (token) headers["Authorization"] = `Bearer ${token}`;
-//   return headers;
-// }
+export function apiUrl(path) {
+  return `${API_BASE}${path}`;
+}
+export function getAuthHeaders(contentType = "application/json") {
+  const headers = {};
+  const token = localStorage.getItem("token");
+  if (contentType) headers["Content-Type"] = contentType;
+  if (token) headers["Authorization"] = `Bearer ${token}`;
+  return headers;
+}
